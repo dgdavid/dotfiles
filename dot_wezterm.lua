3709d7d8d794868bfe1e03ae632c35752ad33793
David Díaz González <diazglezdavid@gmail.com>
2024-10-18 15:43:49 +0100

    wezterm: do not hide mouse cursor

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -9,6 +9,9 @@ local config = wezterm.config_builder()
 -- For example, changing the color scheme:
 config.color_scheme = 'AdventureTime'
 
+-- Do not hide mouse cursor
+config.hide_mouse_cursor_when_typing = false
+
 -- Font
 config.font_size = 13.3
 
